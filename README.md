f70f2375ccb3085cf28465e1182052c95893042e
Tococo852 <tococo852@gmail.com>
2025-08-01 10:36:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+*basic webpack template*
+
+a very basic webpack template for myself, has some basic build/dev/deploy scripts, some basic webpack configuration, and a css reset
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import './styles.css';
+
+if (process.env.NODE_ENV !== 'production') {
+
+   console.log('Looks like we are in development mode!');
+
+ }
+
+const changeClass= (newClass,builder)=>{
+    const content = document.getElementById('content');
+    let currClass= content.className
+    if(currClass!=newClass){
+        content.innerHTML=''
+        content.classList.replace(currClass, newClass);
+        builder()
+    }
+}
+
+document.addEventListener('click', (e)=>{
+    if(e.target.tagName==='BUTTON'){
+         
+    }
+});
+
+   window.addEventListener("DOMContentLoaded", function() {
+        // on load, sethome should run by itself so the page is not blank
+        
+
+
+    }, false);
